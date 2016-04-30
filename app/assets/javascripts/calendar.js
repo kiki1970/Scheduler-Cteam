88a3bd0d0473f35b7fd8a0cc95f85f9b777e6aab
jupiter <jupiter@1970kiki>
2016-04-30 19:14:08 +0900

    イベント編集、削除機能仮完成

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -11,11 +11,47 @@ $(document).ready(function() {
     	$("#event_dialog").dialog("open");
     	$('#event_form [name=start]').val(st);
     	$('#event_form [name=end]').val(en);
-    	console.log(allDay);
     	if(allDay){
     		$('#event_form [name=allDay]').prop("checked",true);
     	}
        	calendar.fullCalendar('unselect');
+	};
+	
+	var edit = function(event, jsEvent, view){
+		var st=moment(event.start).format('YYYY-MM-DDTHH:mm');
+    	$("#edit_dialog").find("textarea, :text, select").val("").end().find(":checked").prop("checked", false);
+    	$('#edit_form [name=id]').hide();
+    	$("#edit_dialog").dialog("open");
+    	console.log(event.end);
+    	$('#edit_form [name=start]').val(st);
+    	if(event.end!=null){
+    		var en=moment(event.end).format('YYYY-MM-DDTHH:mm');
+      		$('#edit_form [name=end]').val(en);  		
+    	}else $('#edit_form [name=end]').val(st);
+    	$('#edit_form [name=id]').val(event.id);
+		$('#edit_form [name=title]').val(event.title);
+		$('#edit_form [name=location]').val(event.location);
+		$('#edit_form [name=remarks]').val(event.remarks);
+    	if(event.allDay){
+    		$('#edit_form [name=allDay]').prop("checked",true);
+    	}
+	};
+	
+	var updateEvent = function(event){
+		var data = {event: {title: event.title,
+                	start: moment(event.start).format('YYYY-MM-DDTHH:mm'),
+                	end: moment(event.end).format('YYYY-MM-DDTHH:mm'),
+                	location: event.location,
+                	remarks: event.remarks,
+                	allDay: event.allDay}};
+    	$.ajax({
+        	type: "PUT",
+        	url: "/events/"+event.id+".json",
+        	data: data,
+        	success: function() {
+           		$("#calendar").fullCalendar('refetchEvents');
+        	}
+     	});
 	};
 
 	var calendar = $('#calendar').fullCalendar({
@@ -23,10 +59,21 @@ $(document).ready(function() {
 		axisFormat: 'H:mm',
 		timeFormat: 'H:mm',
 		header:{right: 'agendaWeek month today prev next'},
-		events: '/events.json',
+		events:{
+			url:'/events.json',
+			success:function(events){
+				$(events).each(function(){
+					this.url = null;
+				});		
+			},
+		},
+		editable: true,
         selectable: true,
         selectHelper: true,
         ignoreTimezone: false,
-        select: select
+        select: select,
+        eventClick: edit,
+        eventResize: updateEvent,
+        eventDrop: updateEvent
    });
 });
--- app/assets/javascripts/dialog.js
+++ app/assets/javascripts/dialog.js
@@ -6,7 +6,7 @@ $(function() {
         width:250,
         resizable:false,
         buttons:{
-            "OK":function(){
+            "作成":function(){
 				var title = $('#event_form [name=title]').val();
 				var start = $('#event_form [name=start]').val();
 				console.log(start);
@@ -25,12 +25,60 @@ $(function() {
             		url: "/events",
             		data: data,
             		success: function() {
-                	$("#calendar").fullCalendar('refetchEvents');
-            	}
+                		$("#calendar").fullCalendar('refetchEvents');
+            		}
         		});
                 $(this).dialog("close");
             },
-            "Cancel":function(){
+            "キャンセル":function(){
+                $(this).dialog("close");
+            }
+        }
+    });
+    $( "#edit_dialog" ).dialog({
+    	modal:true,
+        autoOpen:false,
+        height:400,
+        width:250,
+        resizable:false,
+        buttons:{
+        	"削除":function(){
+        		var id = $('#edit_form [name=id]').val();
+        		if(confirm("予定を削除しま\nよろしいですか？")){
+        			$.ajax({
+            			type: "DELETE",
+            			url: "/events/"+id+".json",
+            			success: function() {
+                			$("#calendar").fullCalendar('refetchEvents');
+            			}	
+        			});
+        		}
+        	},
+            "確定":function(){
+				var id = $('#edit_form [name=id]').val();
+				var title = $('#edit_form [name=title]').val();
+				var start = $('#edit_form [name=start]').val();
+                var end = $('#edit_form [name=end]').val();
+                var location = $('#edit_form [name=location]').val();
+                var remarks = $('#edit_form [name=remarks]').val();
+                var allDay = $('#edit_form [name=allDay]:checked').val();
+                var data = {event: {title: title,
+                            start: start,
+                            end: end,
+                            location: location,
+                            remarks: remarks, 
+                            allDay: allDay}};
+                $.ajax({
+            		type: "PUT",
+            		url: "/events/"+id+".json",
+            		data: data,
+            		success: function() {
+                		$("#calendar").fullCalendar('refetchEvents');
+            		}
+        		});
+                $(this).dialog("close");
+            },
+            "キャンセル":function(){
                 $(this).dialog("close");
             }
         }
