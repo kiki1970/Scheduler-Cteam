5aff0fbb3f1e81c48e4e6051e9662efffa1702f1
jupiter <jupiter@1970kiki>
2016-05-24 03:22:28 +0900

    インポート機能一部修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,13 +1,16 @@
 class Event < ActiveRecord::Base
-
   def self.imp(file)
     CSV.foreach(file, headers: true) do |row|
-      # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
-      @event = find_by(id: row["id"]) || new
+    # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
+
+      @event = Event.new
       # CSVからデータを取得し、設定する
       @event.attributes = row.to_hash.slice(*updatable_attributes)
+      @result = Event.find_by("title = ? AND start = ? AND end = ?" ,@event.title,@event.start,@event.end)
+      if @result == nil then
       # 保存する
       @event.save
+      end
     end
   end
 
@@ -16,6 +19,4 @@ class Event < ActiveRecord::Base
     ["title", "start", "end","location", "url1", "url2", "url3", "url4", "url5", "remarks", "allDay"]
   end
 end
-
-
 
